90521dcebdb36bddd4a2c70046fab51644ba7082
michael lim <michael.limnh@gmail.com>
2020-06-06 16:26:23 +0100

    adding person state to add dynamicism

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './components/Person/Person';
 
 class App extends Component {
+  state = {
+    person: [
+      {name: "Mike", age: "27"},
+      {name: "Ashley", age: "54"},
+      {name: "Mitch", age: "24"}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,9 +21,23 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+          <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
+          <Person name={this.state.person[2].name} age={this.state.person[2].age}>
+            {/* add children props within the custom tags to add extra information that can't be passed as a prop */}
+            {/* children props can be any object or primitive, ol/ul, functions, other custom tags etc */}
+            My Hobbies: Swimming, Cooking
+          </Person>
         </p>
       </div>
     );
+
+    // the code above will ger compiled by 'React' library and use a method called .createElement and compiles the returned text above into the text below:
+    // .createElement takes at least 3 args - element tag, js object with className, style etc, innerHTML/innerText
+    // return React.createElement('div', {className: 'App}, React.createElement('h1', null, 'React App Title'))
+    // => <div className="App">
+       //  <h1>React App Title</h1>
+       // </div>
   }
 }
 
