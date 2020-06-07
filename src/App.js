6ccb26ed47da3257d617cd16132dbe4dccea27ad
michael lim <michael.limnh@gmail.com>
2020-06-07 18:23:00 +0100

    using hooks in functional component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './components/Person/Person';
 
-class App extends Component {
-  state = {
+// functional component used with React Hooks
+const app = props => {
+
+  // useState is React hook that lets you manage state in a functional component
+  // useState will always return only 2 elements, first element is always currentState (beforeState and updatedState), second is a function that alloww us to update the State
+  const [ personsState, setPersonsState ] = useState({  
     person: [
       {name: "Mike", age: "27"},
       {name: "Ashley", age: "54"},
       {name: "Mitch", age: "24"}
-    ]
+    ],
+    otherState: "Something else in state",
+  });
+
+  console.log(personsState);
+
+  
+
+  // must include the word Handler if method isn't actively called but assigning as an event handler
+  const switchNameHandler = () => {
+    // setState allows you to change the objects inside 'state'
+    // still gets passed at the updated initialState
+    setPersonsState({
+      person: [
+        {name: "Jimmy", age: "46"},
+        {name: "Barbara", age: "24"},
+        {name: "Donovan", age: "22"}
+      ]
+    })
   }
 
-  render() {
     return (
       <div className="App">
         <header className="App-header">
@@ -20,10 +41,10 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
-          <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
-          <Person name={this.state.person[2].name} age={this.state.person[2].age}>
+          <button onClick={switchNameHandler}>Switch Name</button>
+          <Person name={personsState.person[0].name} age={personsState.person[0].age}/>
+          <Person name={personsState.person[1].name} age={personsState.person[1].age}/>
+          <Person name={personsState.person[2].name} age={personsState.person[2].age}>
             {/* add children props within the custom tags to add extra information that can't be passed as a prop */}
             {/* children props can be any object or primitive, ol/ul, functions, other custom tags etc */}
             My Hobbies: Swimming, Cooking
@@ -38,7 +59,18 @@ class App extends Component {
     // => <div className="App">
        //  <h1>React App Title</h1>
        // </div>
-  }
 }
 
-export default App;
+export default app;
+
+// state is something that extends from components that let you add state to the class component
+  // class App extends Component {
+    // state = {
+    //   person: [
+    //     {name: "Mike", age: "27"},
+    //     {name: "Ashley", age: "54"},
+    //     {name: "Mitch", age: "24"}
+    //   ],
+    //   otherState: "Something else in state",
+    // }
+  // }
